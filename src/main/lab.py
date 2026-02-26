b46827492600647c4b30e556ac402a3ccef39acf
RoryEiffe <roryeiffe8@gmail.com>
2026-02-26 11:45:33 -0500

    Broke down instructions into multiple parts, hoping to make them more intuitive to associates.

--- src/main/lab.py
+++ src/main/lab.py
@@ -12,7 +12,9 @@ def get_user_input_string(prompt):
 def get_user_input_integer(prompt):
     """
     Instead of returning None this method should Get user input as an integer.
-    Hint : use input() and  convert it into int data type using int()
+    Hint : 
+      1. use input()
+      2. convert it into int data type using int()
     :param prompt: The prompt to display to the user.
     :return: The integer entered by the user.
     """
@@ -26,9 +28,9 @@ def get_user_input_integer(prompt):
 def get_user_input_float(prompt):
     """
     Instead of returning None this method should Get user input as a float.
-    Hint : use input() and  convert it into float  data type using float()
-
-
+    Hint : 
+      1. use input()
+      2. convert it into float data type using float()
     :param prompt: The prompt to display to the user.
     :return: The float entered by the user.
     """
